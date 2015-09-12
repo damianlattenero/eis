6b58cb36c752e4779145d3e81398f2245223b850
Damian Lattenero <damianlattenero@gmail.com>
2015-09-12 06:27:27 -0300

    'sum de [9,9] deberia ser uno,ocho' ok

--- chopper/model/chopper.rb
+++ chopper/model/chopper.rb
@@ -10,28 +10,42 @@ class Chopper
     digitos_en_palabras = ''
 
     if list.empty?
-      digitos_en_palabras << 'vacio'
+      return 'vacio'
     else
       sumar_todo = list.reduce(0) { |a, r| a+r }
       digitos_en_numeros = self.lista_digitos(sumar_todo)
 
       digitos_en_numeros.each { |digito|
-        digitos_en_palabras << self.convertir_a_palabra(digito)
+        digitos_en_palabras << self.convertir_a_palabra(digito) << ','
       }
     end
 
-    digitos_en_palabras
+    digitos_en_palabras.chop
   end
 
   def lista_digitos n
-    [n]
+    ret = []
+
+    digito = n
+
+    while digito > 10
+      ret << digito % 10
+      digito = digito.to_i / 10
+    end
+
+    ret << digito
+    ret.reverse
   end
 
   def convertir_a_palabra n
-    case
-      when 1 == n then "uno"
-      when 4 == n then "cuatro"
-      else n
+    case n
+      when 1 then "uno"
+      when 2 then "dos"
+      when 3 then "tres"
+      when 8 then "ocho"
+      when 4 then "cuatro"
+      else n.to_s
+
     end
   end
 
