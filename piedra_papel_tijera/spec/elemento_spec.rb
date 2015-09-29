edea24908515fbed473130ac8d791d3bdeac2afd
Damian Lattenero <damianlattenero@gmail.com>
2015-09-29 19:52:47 -0300

    Test de papel contra todos los elementos funcionando

--- piedra_papel_tijera/spec/elemento_spec.rb
+++ piedra_papel_tijera/spec/elemento_spec.rb
@@ -27,3 +27,27 @@ describe 'Piedra' do
     expect(piedra.juega_con(Mono)).to eq Empato
   end
 end
+
+describe 'Papel' do
+  let(:piedra) {Piedra.new}
+  let(:papel) {Papel.new}
+  let(:tijera) {Tijera.new}
+  let(:mono) {Mono.new}
+
+
+  it 'Papel deberia ganarle a Piedra' do
+    expect(papel.juega_con(Piedra)).to eq Gano
+  end
+
+  it 'Papel deberia perder con Tijera' do
+    expect(papel.juega_con(Tijera)).to eq Perdio
+  end
+
+  it 'Papel deberia empatar con Papel' do
+    expect(piedra.juega_con(Piedra)).to eq Empato
+  end
+
+  it 'Papel deberia perder con Mono' do
+    expect(papel.juega_con(Mono)).to eq Perdio
+  end
+end
