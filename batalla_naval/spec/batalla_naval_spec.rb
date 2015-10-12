99753f8aeac39f0fcc13aed92c861adc403ea5be
Damian Lattenero <damianlattenero@gmail.com>
2015-10-12 18:30:44 -0300

    ubicacion vertical de crucero exitosa

--- batalla_naval/spec/batalla_naval_spec.rb
+++ batalla_naval/spec/batalla_naval_spec.rb
@@ -26,13 +26,20 @@ describe 'ubicacion de crucero' do
     expect(batalla_naval.hay_barco_en?(Posicion.new(2,1))).to be_truthy
   end
 
+  it 'cuando coloco un barco tipo crucero en 1,1 vertical deberia aparecer en la posicion 1,1 y 1,2 del tablero' do
+    @posicion = Posicion.new(1,1)
+    batalla_naval.ubicar_barco_en(1,1, "crucero", "vertical")
+    expect(batalla_naval.hay_barco_en?(@posicion)).to be_truthy
+    expect(batalla_naval.hay_barco_en?(Posicion.new(1,2))).to be_truthy
+  end
+
 
 end
 
 describe 'ubicacion de acorazado' do
   let(:batalla_naval) { BatallaNaval.new }
 
-  it 'cuando coloco un barco tipo crucero en 1,1 horizontal deberia aparecer en la posicion 1,1 y 2,1 3,1 del tablero' do
+  it 'cuando coloco un barco tipo acorazado en 1,1 horizontal deberia aparecer en la posicion 1,1 y 2,1 3,1 del tablero' do
     @posicion = Posicion.new(1,1)
     batalla_naval.ubicar_barco_en(1,1, "acorazado", "horizontal")
     expect(batalla_naval.hay_barco_en?(@posicion)).to be_truthy
