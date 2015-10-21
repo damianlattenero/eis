cf12c91047ba928d32b6c88a6be5dbfe3ce392be
damian <damianlattenero@gmail.com>
2015-10-21 01:15:53 -0300

    test unitarios de Calculadora: Cuando suma 20 y 22, debe dar 42

--- padrino_quickstart/spec/calculadora_spec.rb
+++ padrino_quickstart/spec/calculadora_spec.rb
@@ -4,7 +4,11 @@ require_relative '../../padrino_quickstart/app/models/calculadora'
 
 describe 'Calculadora' do
 
-  it 'sumar correctamente' do
+  it 'sumar 2 y 2 da 4' do
     expect(Calculadora.suma 2, 2).to eq 4
+  end
+
+  it 'sumar 20 y 22 da 42' do
+    expect(Calculadora.suma 22, 20).to eq 42
   end
 end
